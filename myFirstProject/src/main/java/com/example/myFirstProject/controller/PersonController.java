97ba539f19e40db8753f5eff8773bdb2a9999815
Seniha Kulaksız <senihakulaksiz2@gmail.com>
2024-08-13 13:52:41 +0300

    .

--- myFirstProject/src/main/java/com/example/myFirstProject/controller/PersonController.java
+++ myFirstProject/src/main/java/com/example/myFirstProject/controller/PersonController.java
@@ -1,8 +1,11 @@
 package com.example.myFirstProject.controller;
 
+import com.example.myFirstProject.dto.LoginModelDTO;
 import com.example.myFirstProject.model.Person;
 import com.example.myFirstProject.repository.PersonRepository;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +25,24 @@ public class PersonController {
         return _personRepository.findAll();
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginModelDTO model) {
+        Person person = _personRepository.findByName(model.getName());
+        if (person == null) {
+            return ResponseEntity.badRequest().body("Kullanıcı adı ya da şifre hatalı!");
+        }
+        boolean isPasswordMatch = BCrypt.checkpw(model.getPassword(), person.getPassword());
+        if (!isPasswordMatch) {
+            return ResponseEntity.badRequest().body("Kullanıcı adı ya da şifre hatalı!");
+        }
+
+        return ResponseEntity.ok().body("Giriş başarılı");
+    }
+
     @PostMapping
     public Person insertPerson(@RequestBody Person person) {
+        String hashedPwd = BCrypt.hashpw(person.getPassword(), BCrypt.gensalt());
+        person.setPassword(hashedPwd);
         return _personRepository.save(person);
     }
 
---
+++ myFirstProject/src/main/java/com/example/myFirstProject/dto/LoginModelDTO.java
@@ -0,0 +1,9 @@
+package com.example.myFirstProject.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginModelDTO {
+    private String name;
+    private String password;
+}
